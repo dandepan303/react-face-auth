de70a167ff80ac959616d15cab41d04f6f7d019b
Dean Hiranburana <dandepan303@gmail.com>
2024-01-24 19:08:02 +0000

    Change welcome page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,13 @@ function Home() {
             Welcome To
           </h1>
           <h1 className="block text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-indigo-900">
-            React Face Auth
+            Colab Chat 3.0
           </h1>
           <p className="mt-8 text-md text-gray-600 max-w-3xl mx-4 md:mx-16 lg:mx-auto">
-            The Facial Recognition-Based Authentication Application is a
-            cutting-edge web application developed using React and face-api.js.
-            The main objective of the application is to offer a reliable and
-            efficient authentication system by analyzing and verifying the
-            user's facial features.
+            Colab Chat 3.0 is a state-of-the art chat application that uses
+            face authentication for fast and secure passwordless login. No
+            more impersonating people or unauthorized access. Created using
+            React and face-api.js.
           </p>
           <Link
             to={"/user-select"}
